6e266bb48c4d4e2fb530f9d9ef23a861e8efc00d
yamano <48882447+cyberlimbo@users.noreply.github.com>
2020-11-18 09:45:14 +0000

    TeaSpeak Public Script
    -TeaSpeak Server Install
    -TeaSpeak MusicBot Install

---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+if [ "$(whoami)" == "root" ] ; 
+then
+ echo "#############################################################"
+ echo "#                                                           #"
+ echo "#     All-in-one Script patcher by @yamano                  #"
+ echo "#     Debian 8/9/10 and Ubuntu 18/19/20  32 & 64 bits       #"
+ echo "#     Website: CyberLimbo.com                               #"
+ echo "#     Email: support@cyberLimbo.com                         #"
+ echo "#     Patcher version: 1.1-public                           #"
+ echo "#                                                           #"
+ echo "#############################################################"
+ echo "#                                                           #"
+ echo "#     Checking system architecture...                       #"
+ echo "#                                                           #"
+ echo "#############################################################"
+else
+ echo "#############################################################"
+ echo "#                                                           #"
+ echo "#     All-in-one Script patcher by @yamano                  #"
+ echo "#     Debian 8/9/10 and Ubuntu 18/19/20  32 & 64 bits       #"
+ echo "#     Website: CyberLimbo.com                               #"
+ echo "#     Email: support@cyberLimbo.com                         #"
+ echo "#     Patcher version: 1.1-public                           #"
+ echo "#                                                           #" 
+ echo "#############################################################"
+ echo "#                                                           #"
+ echo "#     You're not runnig the script as root user.            #"
+ echo "#     Please run it as root or it will not work!            #" 
+ echo "#                                                           #"
+ echo "#############################################################"
+exit
+fi
+if [ `getconf LONG_BIT` = "64" ] 
+then
+ echo "#                                                           #"
+ echo "#     x64 system detected!                                  #"
+ echo "#                                                           #"
+ echo "#############################################################"
+ echo "#                                                           #"
+ echo "#     Downloading x64 patcher...                            #"
+ echo "#                                                           #"
+ rm -f /root/installx64  
+ wget -q https://tools.cyberlimbo.com/scripts/installx64 -P /root
+ chmod 777 /root/installx64
+ exec /root/installx64
+else
+ echo "#                                                           #"
+ echo "#     x86 system detected!                                  #"
+ echo "#                                                           #"
+ echo "#############################################################"
+ echo "#                                                           #"
+ echo "#     Downloading x86 patcher...                            #"
+ echo "#                                                           #"
+ rm -f /root/installx86 
+ wget -q https://tools.cyberlimbo.com/scripts/installx86 -P /root
+ chmod 777 /root/installx86
+ exec /root/installx86
+fi
+exit
